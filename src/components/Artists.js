7b14e7501ee6aeeb2ed4f94aafc3ca2e0883fe28
Kevin Brown <kevinbrown367@gmail.com>
2020-08-01 19:24:26 -0600

    Artists and Profiles

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,9 +1,25 @@
 import React from "react";
+import ARTISTSPROFILES from "../data/artistsProfiles";
 
 function Artists() {
   return (
     <div>
-      <p>Artists</p>
+      <h2>Artists of the Ruberto Ostberg Gallery</h2>
+      <h4>
+        The Ruberto Ostberg Gallery is host to some of the finest artists from
+        the Calgary area and around the world.
+      </h4>
+      <div>
+        <div>
+          {ARTISTSPROFILES.map((ARTISTPROFILE) => {
+            return (
+              <div>
+                <p>{ARTISTPROFILE.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
